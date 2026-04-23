6616dd8da146279f7cd2e591f111323fc729bbea
Luana <luana.suarez@alu.frlp.utn.edu.ar>
2026-04-23 17:34:31 +0000

    chore(database): agrega script init.sql con tabla members

---
+++ database/init.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS members;
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    legajo VARCHAR(20) NOT NULL UNIQUE,
+    feature VARCHAR(50) NOT NULL,
+    servicio VARCHAR(50) NOT NULL,
+    estado VARCHAR(30) NOT NULL
+);
+
+INSERT INTO members (nombre, apellido, legajo, feature, servicio, estado)
+VALUES
+    ('Maria Luana', 'Suarez Pavicicho', '33210O', 'Feature 04', 'database', 'running'),
+    ('Macarena', 'Romero Olmo ', '33372', 'Feature 02', 'frontend', 'running'),
+    ('Valentina', 'Pertila de la Vega', '33288', 'Feature 03', 'backend', 'running'),
+    ('Valentina', 'Pertile de la Vega', '33288', 'Feature 01', 'compose/readme', 'running'),
+    ('Macarena', 'Romero Olmo', '33372', 'Feature 05', 'portainer', 'running');
